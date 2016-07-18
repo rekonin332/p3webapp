c11581c4e50835f411b400d3f6a6c12f750b8638
Todd Tse <38468856@qq.com>
2016-07-18 11:06:52 +0800

    test commit in vscode

--- www/test3.py
+++ www/test3.py
@@ -9,7 +9,7 @@ from www.models import User, Blog, Comment
 #     u = User(name='victoria', email='457855140@qq.com', passwd='1234567890', image='about:blank')
 #
 #     yield from u.save()
-
+#
 @asyncio.coroutine
 def test(loop):
     yield from www.orm.create_pool(loop=loop, host='localhost', port=3306, user='root', password='123456', db='awesome')
@@ -21,3 +21,18 @@ loop.run_until_complete(test(loop))
 loop.close()
 if loop.is_closed():
     sys.exit(0)
+
+
+
+# import orm
+# from models import User, Blog, Comment
+#
+# def test():
+#     yield from orm.create_pool(user='root', password='123456', database='awesome')
+#
+#     u = User(name='Test#', email='test#@example.com', passwd='1234567890', image='about:blank')
+#
+#     yield from u.save()
+#
+# for x in test():
+#     pass
